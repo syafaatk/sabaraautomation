6197753d5cadfd41125183dc2f3f5434663cc3ca
Khoirusy Syafaat <syafaatkhoirusy@gmail.com>
2025-01-09 11:59:28 +0700

    change read more to hubungi kami

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -24,7 +24,7 @@ foreach ($slider as $slider) {  ?>
           <div class="container" style="max-width: 70%; text-align: left; padding-left: 2%; padding-right: 2%;">
                 <h2><?= $slider['judul_galeri'] ?></h2>
                 <p><?= $slider['isi'] ?></p>
-                <a href="<?= $slider['website'] ?>" class="btn-get-started scrollto">Read More</a>
+                <a href="<?= $slider['website'] ?>" class="btn-get-started scrollto">Hubungi Kami</a>
             </div>
           <?php } ?>
         </div>
